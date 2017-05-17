3a52b5b33b1b815b0d047e32e59046bbf168acbf
ivanvachev <vanchok2000@gmail.com>
2017-05-18 00:53:00 +0600

    Текстуры, кнопки, scene2d

--- core/src/ru/codemonkeystudio/screens/MainMenuScreen.java
+++ core/src/ru/codemonkeystudio/screens/MainMenuScreen.java
@@ -8,7 +8,10 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import ru.codemonkeystudio.game.MyGdxGame;
 
@@ -25,27 +28,33 @@ public class MainMenuScreen implements Screen {
 	private Texture texture;
 	private Texture[] btnActive, btnInactive;
 	private Texture logo;
-	private TextureRegion[] button;
 	private int selection, cursorX, cursorY;
 	private Sound sound;
+	private Image icon;
+	private Texture textureIcon;
+	private Stage stage;
+	private ImageButton button;
+	private ImageButton.ImageButtonStyle buttonStyle;
 
 	public MainMenuScreen(MyGdxGame game) {
+		stage = new Stage();
 		this.game = game;
 		batch = new SpriteBatch();
 		logo = new Texture("gui/Terrarum.png");
-		//gamecam = new OrthographicCamera();
-		//gamePort = new FitViewport(800, 480, gamecam);
+		gamecam = new OrthographicCamera();
+
+		gamePort = new FitViewport(800, 600, gamecam);
 
 		btnActive = new Texture[4];
 		btnInactive = new Texture[4];
 
 		btnActive[0] = new Texture("gui/newGame_ac.png");
-		btnActive[1] = new Texture("gui/achievments_ac.png");
+		btnActive[1] = new Texture("gui/achievements_ac.png");
 		btnActive[2] = new Texture("gui/settings_ac.png");
 		btnActive[3] = new Texture("gui/exit_ac.png");
 
 		btnInactive[0] = new Texture("gui/newGame_ps.png");
-		btnInactive[1] = new Texture("gui/achievments_ps.png");
+		btnInactive[1] = new Texture("gui/achievements_ps.png");
 		btnInactive[2] = new Texture("gui/settings_ps.png");
 		btnInactive[3] = new Texture("gui/exit_ps.png");
 		selection = -1;
@@ -64,30 +73,36 @@ public class MainMenuScreen implements Screen {
 	public void render(float delta) {
 		Gdx.gl20.glClearColor(0, 0, 0, 1);
 		Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		//batch.setProjectionMatrix(gamecam.combined);
+		gamecam.position.x = 0;
+		gamecam.position.y = 0;
+		gamecam.update();
+		batch.setProjectionMatrix(gamecam.combined);
+
+
+
+
+
+		textureIcon = new Texture(Gdx.files.internal("gui/Terrarum.png"));
+		icon = new Image(textureIcon);
+		icon.setPosition(stage.getWidth()/2 - icon.getWidth()/2 ,stage.getHeight() - icon.getHeight()*5);
+		stage.addActor(icon);
+
+
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 
 		batch.begin();
-		batch.draw(logo, Gdx.graphics.getWidth() / 2 - logo.getWidth() / 2 * 10, Gdx.graphics.getHeight() - logo.getHeight() * 10 - 10, logo.getWidth() * 10, logo.getHeight() * 10);
 
-		if (selection == 0) batch.draw(btnActive[0], Gdx.graphics.getWidth() / 2 - btnActive[0].getWidth() / 2 * 4, Gdx.graphics.getHeight() - btnActive[0].getHeight() * 10 - 10 - btnActive[0].getHeight() * 4, btnActive[0].getWidth() * 4, btnActive[0].getHeight() * 4);
-		else  batch.draw(btnInactive[0], Gdx.graphics.getWidth() / 2 - btnActive[0].getWidth() / 2 * 4, Gdx.graphics.getHeight() - btnActive[0].getHeight() * 10 - 10 - btnActive[0].getHeight() * 4, btnActive[0].getWidth() * 4, btnActive[0].getHeight() * 4);
-		if (selection == 1) batch.draw(btnActive[1], Gdx.graphics.getWidth() / 2 - btnActive[0].getWidth() / 2 * 4, Gdx.graphics.getHeight() - btnActive[0].getHeight() * 10 - 10 - btnActive[0].getHeight() * 4 * 2, btnActive[0].getWidth() * 4, btnActive[0].getHeight() * 4);
-		else  batch.draw(btnInactive[1], Gdx.graphics.getWidth() / 2 - btnActive[0].getWidth() / 2 * 4, Gdx.graphics.getHeight() - btnActive[0].getHeight() * 10 - 10 - btnActive[0].getHeight() * 4 * 2, btnActive[0].getWidth() * 4, btnActive[0].getHeight() * 4);
-		if (selection == 2) batch.draw(btnActive[2], Gdx.graphics.getWidth() / 2 - btnActive[0].getWidth() / 2 * 4, Gdx.graphics.getHeight() - btnActive[0].getHeight() * 10 - 10 - btnActive[0].getHeight() * 4 * 3, btnActive[0].getWidth() * 4, btnActive[0].getHeight() * 4);
-		else  batch.draw(btnInactive[2], Gdx.graphics.getWidth() / 2 - btnActive[0].getWidth() / 2 * 4, Gdx.graphics.getHeight() - btnActive[0].getHeight() * 10 - 10 - btnActive[0].getHeight() * 4 * 3, btnActive[0].getWidth() * 4, btnActive[0].getHeight() * 4);
-		if (selection == 3) batch.draw(btnActive[3], Gdx.graphics.getWidth() / 2 - btnActive[0].getWidth() / 2 * 4, Gdx.graphics.getHeight() - btnActive[0].getHeight() * 10 - 10 - btnActive[0].getHeight() * 4 * 4, btnActive[0].getWidth() * 4, btnActive[0].getHeight() * 4);
-		else  batch.draw(btnInactive[3], Gdx.graphics.getWidth() / 2 - btnActive[0].getWidth() / 2 * 4, Gdx.graphics.getHeight() - btnActive[0].getHeight() * 10 - 10 - btnActive[0].getHeight() * 4 * 4, btnActive[0].getWidth() * 4, btnActive[0].getHeight() * 4);
+		if(selection == 0) batch.draw(btnActive[0], 0, 70);
+		else  batch.draw(btnInactive[0], 0, 70);
+		if(selection == 1) batch.draw(btnActive[1], 0, 50);
+		else  batch.draw(btnInactive[1], 0, 50);
+		if(selection == 2) batch.draw(btnActive[2], 0, 30);
+		else  batch.draw(btnInactive[2], 0, 30);
+		if(selection == 3) batch.draw(btnActive[3], 0, 10);
+		else  batch.draw(btnInactive[3], 0, 10);
 
-//		if(selection == 0) batch.draw(btnActive[0], Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//		else  batch.draw(btnInactive[0], Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//		if(selection == 1) batch.draw(btnActive[1], Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//		else  batch.draw(btnInactive[1], Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//		if(selection == 2) batch.draw(btnActive[2], Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//		else  batch.draw(btnInactive[2], Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//		if(selection == 3) batch.draw(btnActive[3], Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-//		else  batch.draw(btnInactive[3], Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-				batch.end();
+		batch.end();
 
 		if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
 			selection--;
@@ -157,7 +172,7 @@ public class MainMenuScreen implements Screen {
 
 	@Override
 	public void resize(int width, int height) {
-		//gamePort.update(width, height);
+		gamePort.update(width, height);
 	}
 
 	@Override
--- core/src/ru/codemonkeystudio/screens/SettingsScreen.java
+++ core/src/ru/codemonkeystudio/screens/SettingsScreen.java
@@ -8,61 +8,94 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import ru.codemonkeystudio.game.MyGdxGame;
 
 /**
  * Created by IOne on 23.04.2017.
  */
-public class SettingsScreen implements Screen{
+public class SettingsScreen implements Screen {
+
     private MyGdxGame game;
+    private Image icon;
+    private Texture textureIcon;
+    private Stage stage;
+    private ImageButton button;
+    private ImageButton.ImageButtonStyle buttonStyle;
     private SpriteBatch batch;
-    private ShapeRenderer renderer;
+    private BitmapFont font;
+    private Label label;
+    private TextureAtlas atlas;
+    private Skin skin;
+    private Table table;
+    private Image image;
+
     private OrthographicCamera gamecam;
     private Viewport gamePort;
 
-    private Texture[] btnSActive, btnSInactive;
-    public static BitmapFont font;
-
-
     public SettingsScreen(MyGdxGame game) {
         gamecam = new OrthographicCamera();
-        this.renderer = renderer;
-        this.game = game;
+        gamePort = new FillViewport(800, 600, gamecam);
+
         batch = new SpriteBatch();
-        gamePort = new ScreenViewport(gamecam);
+        font = new BitmapFont(Gdx.files.internal("fonts/Terrarum.fnt"),Gdx.files.internal("fonts/Terrarum.png"), false);
 
-        String FONT_CHARS = "";
-        for( int i = 32; i < 127; i++ ) FONT_CHARS += (char)i; // цифры и весь английский
-        for( int i = 1024; i < 1104; i++ ) FONT_CHARS += (char)i; // русские
+        this.game = game;
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        label = new Label("Settings", new Label.LabelStyle(font, Color.WHITE));
+        label.setPosition(0, 0);
+        stage.addActor(label);
+        atlas = new TextureAtlas("Textures/Texture_ui.atlas");
+        skin = new Skin(atlas);
 
-        final String FONT_PATH = "fonts/terrarum.ttf";
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(FONT_PATH));
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.characters = FONT_CHARS;
-        parameter.size = 30;
-        parameter.color = Color.WHITE;
-        font = generator.generateFont(parameter);
-        generator.dispose();
+        buttonStyle = new ImageButton.ImageButtonStyle();
+        buttonStyle.up = skin.getDrawable("btn_unactive");//кнопка не нажата
+        buttonStyle.over = skin.getDrawable("btn_active");
+        buttonStyle.down = skin.getDrawable("btn_pressed"); // кнопка нажата
 
-
-
+        button = new ImageButton(buttonStyle);
+        button.add(label);
+        button.setSize(100, 100);// размер кнопки
+        button.setPosition(stage.getWidth()/2 - button.getWidth()/2, stage.getHeight()/2 - button.getHeight()/2); // позиция кнопки(с нижнего левого угла координаты считаются)
+        button.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("pressed");
+                return true;
+            }
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                System.out.println("unpressed");
+            }
+        });
+        stage.addActor(button); //добавляем кнопку к сцене
     }
 
     @Override
     public void show () {
 
+
+
     }
 
     @Override
     public void render ( float delta){
-        Gdx.gl20.glClearColor(0, 0, 0, 1);
-        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0,0,0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gamecam.position.x = 0;
+        gamecam.position.y = 0;
+        gamecam.update();
         batch.setProjectionMatrix(gamecam.combined);
 
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
 
     }
 
